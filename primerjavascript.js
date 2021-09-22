915435694c1ad0afcd216bd7bb7b92e3a685a9f7
Nemesisrf <genesiisrojjass@gmail.com>
2021-09-21 19:06:44 -0500

    primer commit

---
+++ primerjavascript.js
@@ -0,0 +1,28 @@
+alert("¡Bienvenidos a mi paginas de Radiohead!");
+
+console.log("errores");
+
+const nombreAlbum1 = () => {
+  alert("Ok Computer");
+};
+
+const nombreAlbum2 = () => {
+    alert("Kid A");
+  };
+
+  const nombreAlbum3 = () => {
+    alert("Amnesiac");
+  };
+
+  const nombreAlbum4 = () => {
+    alert("In Rainbows");
+  };
+
+  const nombreAlbum5 = () => {
+    alert("Pablo Honey");
+  };
+
+  const nombreAlbum6 = () => {
+    alert("The Bends");
+  };
+
